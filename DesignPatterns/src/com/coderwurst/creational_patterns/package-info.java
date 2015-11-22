f1d342123d255f84289350929f2cc9d1ffd82f39
coderwurst <coderwurst@gmail.com>
2015-11-22 10:41:00 +0000

    Initial Commit
    Exercises to Accompany Java Design Patterns, Bevis 2012

---
+++ DesignPatterns/src/com/coderwurst/creational_patterns/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * @author coderwurst
+ * Bevis (2012), Java Design Patterns Essentials Exercises
+ * http://www.amazon.co.uk/Java-Design-Pattern-Essentials-Second/dp/0956575846/ref=sr_1_sc_1?ie=UTF8&qid=1448188704&sr=8-1-spell&keywords=design+patterns+essentai
+ */
+package com.coderwurst.creational_patterns;
